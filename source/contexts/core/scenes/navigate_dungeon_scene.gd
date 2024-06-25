e8a1a51a747a3c14b1f2d30cec03f72f96bc097b
nightblade9 <nightbladecodes@gmail.com>
2024-06-25 16:53:33 -0400

    Map generation and population, including fog.

--- source/contexts/core/scenes/navigate_dungeon_scene.gd
+++ source/contexts/core/scenes/navigate_dungeon_scene.gd
@@ -1,5 +1,66 @@
 extends Node2D
 
+const Player = preload("res://contexts/core/scenes/entities/Player.tscn")
+
+const MAP_SIZE_TILES = Vector2i(50, 50)
+
+const TILE_LAYERS = {
+	"structure": 0,
+	"interactive": 1, # entities, that aren't player/monsters.
+	"fog": 2
+}
+
+# TODO: populate programmatically
+const TILE_NAME_TO_OFFSET = {
+	"ground": Vector2i.ZERO,
+	"wall": Vector2i.RIGHT,
+	"fog": Vector2i.RIGHT * 2
+}
+
+var _tile_data = Array2D.new(MAP_SIZE_TILES.x, MAP_SIZE_TILES.y)
+var _entity_data = Array2D.new(MAP_SIZE_TILES.x, MAP_SIZE_TILES.y)
+var _fog_data = Array2D.new(MAP_SIZE_TILES.x, MAP_SIZE_TILES.y) # coordinates => is_fogged
+
+var _player_start = Vector2i(6, 3)
+
 func _ready():
-	pass
+	# TODO: move out
+	_generate_dungeon()
 	
+	_populate_tiles()
+	_populate_entities()
+
+func _generate_dungeon():
+	_tile_data.fill("ground")
+	
+	for x in range(MAP_SIZE_TILES.x):
+		_tile_data.set_at(x, 0, "wall")
+		_tile_data.set_at(x, MAP_SIZE_TILES.y - 1, "wall")
+	
+	for y in range(MAP_SIZE_TILES.y):
+		_tile_data.set_at(0, y, "wall")
+		_tile_data.set_at(MAP_SIZE_TILES.x - 1, y, "wall")
+	
+	for y in range(MAP_SIZE_TILES.y):
+		for x in range(MAP_SIZE_TILES.x):
+			_fog_data.set_at(x, y, true) # true => is_fogged
+	
+func _populate_tiles():
+	%DungeonTileMap.clear()
+	
+	for y in range(MAP_SIZE_TILES.y):
+		for x in range(MAP_SIZE_TILES.x):
+			var tile_name = _tile_data.get_at(x, y)
+			var atlas_coordinates = TILE_NAME_TO_OFFSET[tile_name]
+			%DungeonTileMap.set_cell(TILE_LAYERS["structure"], Vector2i(x, y), 0, atlas_coordinates)
+			
+			var is_fogged = _fog_data.get_at(x, y)
+			if is_fogged:
+				%DungeonTileMap.set_cell(TILE_LAYERS["fog"], Vector2i(x, y), 0, TILE_NAME_TO_OFFSET["fog"])
+
+func _populate_entities():
+	var player = Player.instantiate()
+	add_child(player)
+	player.position = _player_start * 32
+	# Data moves to synch with scene, to start. After this, data drives it.
+	_entity_data.set_at(_player_start.x, _player_start.y, player)
