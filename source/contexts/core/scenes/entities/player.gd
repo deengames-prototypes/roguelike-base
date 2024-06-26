1c7077174e1193471d533aef42c92ed9639daeed
nightblade9 <nightbladecodes@gmail.com>
2024-06-26 16:46:51 -0400

    Hmm. Player saved-data goes where?

--- source/contexts/core/scenes/entities/player.gd
+++ source/contexts/core/scenes/entities/player.gd
@@ -1,5 +1,7 @@
 class_name Player
-extends CharacterBody2D
+extends Node2D
 
 var sight_radius:int = 3
 var is_tweening:bool = false
+
+# TODO: fields that we save, go in a Resource that's in here? Or in SaveData? idk.
