299d0f6d4bc6e2a7b4701941177744c0f0be5607
nightblade9 <nightbladecodes@gmail.com>
2024-07-07 23:17:25 -0400

    Solved: don't move into stationaries.
    Cause? Ray2D didn't collide with areas.

--- source/contexts/core/scripts/BaseEntity.gd
+++ source/contexts/core/scripts/BaseEntity.gd
@@ -12,8 +12,8 @@ static var moving_next_turn:Array2D = Array2D.new(1000, 1000)
 func _ready():
 	ray = %RayCast2D
 
-func can_move(direction:Vector2i) -> bool:
-	ray.target_position = direction * TILE_SIZE
+func can_move(direction:Vector2) -> bool:
+	ray.target_position = direction.normalized() * TILE_SIZE
 	ray.force_raycast_update()
 	return not ray.is_colliding()
 
@@ -25,11 +25,9 @@ func move(direction:Vector2i) -> bool:
 	
 	# Check if we can move, or if someone else is moving there
 	if moving_next_turn.get_at(tile_destination):
-		print("%s last-minute cancel at %s, occupied by %s" % [self.name, tile_destination, moving_next_turn.get_at(tile_destination).name])
 		return false
 	
 	moving_next_turn.set_at(tile_destination, self)
-	print("%s is moving to %s" % [self, tile_destination])
 	var tween = create_tween()
 	tween.tween_property(self, "position", destination, MOVE_TIME_SECONDS).set_trans(Tween.TRANS_SINE)
 	
