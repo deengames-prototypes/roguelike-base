8f68c1550f0871c297839316c4ef585eb409c6fa
nightblade9 <nightbladecodes@gmail.com>
2024-07-09 17:20:18 -0400

    Primitive, but working, aiming reticule.
    Ray2D didn't work within a shape, so Area2D for the win.

--- source/contexts/core/scenes/ui/Aimer.gd
+++ source/contexts/core/scenes/ui/Aimer.gd
@@ -1,6 +1,38 @@
 extends Node2D
 
-signal picked_target(who)
+signal picked_target(who) # null = cancelled
+
+const TILE_SIZE:int = 32
+
+var player
+var target
+
+var direction_vectors = {
+	"right": Vector2.RIGHT,
+	"left": Vector2.LEFT,
+	"up": Vector2.UP,
+	"down": Vector2.DOWN
+}
+
+func _ready() -> void:
+	player = get_tree().get_first_node_in_group("Player")
 
 func _process(_delta: float) -> void:
-	pass
+	if Input.is_action_just_pressed("ui_cancel"):
+		picked_target.emit(null) # cancelled
+	
+	for direction in direction_vectors:
+		if Input.is_action_just_pressed("ui_%s" % direction):
+			self.position += direction_vectors[direction] * TILE_SIZE
+	
+	if Input.is_action_just_pressed("fire_projectile") and target != null and target != player:
+		picked_target.emit(target)
+
+func _on_area_entered(area: Area2D) -> void:
+	if area == player:
+		return
+		
+	target = area
+
+func _on_area_exited(area: Area2D) -> void:
+	target = null
