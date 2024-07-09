d71fe55212cd29c74698c0c5da7c0b1e6c4fd7cf
nightblade9 <nightbladecodes@gmail.com>
2024-07-09 17:04:11 -0400

    Preparation for aiming: we add the aimer.
    Refactor player code to break out lots of little functions.

--- source/contexts/core/scenes/entities/Player.gd
+++ source/contexts/core/scenes/entities/Player.gd
@@ -1,6 +1,8 @@
 class_name Player
 extends BaseEntity
 
+const Aimer = preload("res://contexts/core/scenes/ui/Aimer.tscn")
+
 var direction_vectors = {
 	"right": Vector2.RIGHT,
 	"left": Vector2.LEFT,
@@ -13,11 +15,15 @@ var direction_vectors = {
 # bow-range, or spell-range to spell-range, we're still hittable.
 var moving_to_tile:Vector2i
 
+var is_aiming:bool = false
+
+
 # TODO: GOES INTO DATA
 var health_left:int = 4
+var firing_range:int = 5 # 0 to disable
 
-func _unhandled_key_input(event: InputEvent) -> void:
-	if is_moving:
+func _unhandled_key_input(event:InputEvent) -> void:
+	if is_moving or is_aiming:
 		return
 	
 	# Clear who is moving where this turn
@@ -25,27 +31,11 @@ func _unhandled_key_input(event: InputEvent) -> void:
 	
 	if Input.is_action_just_pressed("ui_accept"):
 		pass_turn()
+	
+	if firing_range > 0 and Input.is_action_just_pressed("fire_projectile"):
+		handle_aim_and_fire()
 
-	for direction in direction_vectors.keys():
-		var direction_vector:Vector2i = direction_vectors[direction]
-		if event.is_action_pressed("ui_%s" % direction):
-			if not can_move(direction_vector):
-				var whos_there = get_melee_target(direction_vector)
-				if whos_there != null:
-					# MELEE MELEE MELEEEEEEEEEEE
-					whos_there.hurt()
-					var tween = ActionTweener.attack(create_tween(), self, direction_vector)
-					await tween.finished
-					pass_turn()
-					break
-				
-				# Blocked but not hittable/breakable, e.g. wall
-				continue
-				
-			# First spot that's walkable
-			self.moving_to_tile = Vector2i(self.position / TILE_SIZE) + direction_vector
-			move(direction_vector)
-			break
+	handle_movement(event)
 
 func pass_turn():
 	move(Vector2.ZERO)
@@ -61,3 +51,39 @@ func hurt():
 	if self.health_left <= 0:
 		CoreEventBus.player_died.emit()
 		self.queue_free()
+
+func handle_movement(event: InputEvent) -> void:
+	for direction in direction_vectors.keys():
+		var direction_vector:Vector2i = direction_vectors[direction]
+		if event.is_action_pressed("ui_%s" % direction):
+			if not can_move(direction_vector):
+				var whos_there = get_melee_target(direction_vector)
+				if whos_there != null:
+					handle_melee(whos_there, direction_vector)
+					return
+					
+				# Blocked but not hittable/breakable, e.g. wall
+				continue
+				
+			# First spot that's walkable
+			self.moving_to_tile = Vector2i(self.position / TILE_SIZE) + direction_vector
+			move(direction_vector)
+			break
+
+func handle_aim_and_fire() -> void:
+	is_aiming = true
+	var aimer = Aimer.instantiate()
+	aimer.position = self.position
+	
+	get_parent().add_child(aimer) # starts aiming automagically
+	var target = await aimer.picked_target
+	
+	get_parent().remove_child(aimer)
+	print("returned %s" % target)
+	is_aiming = false
+
+func handle_melee(target:Monster, direction_vector:Vector2i) -> void:
+	target.hurt()
+	var tween = ActionTweener.attack(create_tween(), self, direction_vector)
+	await tween.finished
+	pass_turn()
---
+++ source/contexts/core/scenes/ui/Aimer.gd
@@ -0,0 +1,6 @@
+extends Node2D
+
+signal picked_target(who)
+
+func _process(_delta: float) -> void:
+	pass
